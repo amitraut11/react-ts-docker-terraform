2d322a526a73e578f6edc773d198263e748c7187
amit.raut <amitraut11@gmail.com>
2024-02-13 22:09:19 -0600

    added new filesss14

--- pipeline.tf
+++ pipeline.tf
@@ -96,20 +96,20 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
 
 
-    stage {
-        name ="Apply"
-        action{
-            name = "Deploy"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd-apply"
-            }
-        }
-    }
+    # stage {
+    #     name ="Apply"
+    #     action{
+    #         name = "Deploy"
+    #         category = "Build"
+    #         provider = "CodeBuild"
+    #         version = "1"
+    #         owner = "AWS"
+    #         input_artifacts = ["tf-code"]
+    #         configuration = {
+    #             ProjectName = "tf-cicd-apply"
+    #         }
+    #     }
+    # }
 
    
 
