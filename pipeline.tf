d23690ab2badac194c1326decae96e66951ad118
amit.raut <amitraut11@gmail.com>
2024-02-14 16:53:04 -0600

    added new filesss14

--- pipeline.tf
+++ pipeline.tf
@@ -48,6 +48,38 @@ resource "aws_codebuild_project" "tf-apply" {
  }
 }
 
+resource "aws_codebuild_project" "tf-image" {
+  name          = "tf-cicd-image"
+  description   = "Builds a Docker image and pushes it to ECR"
+  build_timeout = 60
+  service_role  = aws_iam_role.tf-codebuild-role-terraform.arn
+ 
+source {
+     type   = "CODEPIPELINE"
+     buildspec = file("image-buildspec.yml")
+ }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:5.0"  # Docker image with Terraform and other tools
+    type         = "LINUX_CONTAINER"
+    privileged_mode = true
+    #image_pull_credentials_type = "SERVICE_ROLE"
+    environment_variable {
+      name  = "DOCKER_REPO"
+      value = "https://hub.docker.com/u/amitraut11"  # Replace with your Docker repository URL
+    }
+    # registry_credential{
+    #     credential = var.dockerhub_credentials
+    #     credential_provider = "SECRETS_MANAGER"
+    # }
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+}
+
 
 
 
@@ -107,6 +139,21 @@ resource "aws_codepipeline" "cicd_pipeline" {
             input_artifacts = ["tf-code"]
             configuration = {
                 ProjectName = "tf-cicd-apply"
+            }
+        }
+    }
+
+    stage {
+        name ="Image"
+        action{
+            name = "Image"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "tf-cicd-image"
             }
         }
     }
